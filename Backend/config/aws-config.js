d2808900f31bd7e2f0cbd2a14e55d188cb16da69
Navilrc <imailnaveenaks@gmail.com>
2026-01-18 23:34:02 +0530

    configure aws credientials

--- Backend/config/aws-config.js
+++ Backend/config/aws-config.js
@@ -1,8 +1,8 @@
  const AWS = require("aws-sdk");
 
-AWS.config.update({ region: "eu-north-1" });
+AWS.config.update({ region: "ap-south-1" });
 
 const s3 = new AWS.S3();
-const S3_BUCKET = "samplemybuckett9136 ";
+const S3_BUCKET = "samplemybucket-vcs-8090";
 
 module.exports = { s3, S3_BUCKET };
